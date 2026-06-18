026886b8feadcd15a0a66bedf43928f63fd55fc9
Angular <gustavohenriquelima1@hotmail.com>
2026-06-17 22:20:42 -0300

    modified:   components/wifi/CMakeLists.txt 	modified:   components/wifi/include/wifi.h

--- components/wifi/include/wifi.h
+++ components/wifi/include/wifi.h
@@ -1,1 +1,24 @@
-void func(void);
+#ifndef WIFI_H
+#define WIFI_H
+
+#include "esp_wifi.h"
+
+#if __cplusplus
+extern "C"
+{
+#endif
+
+/** @brief Initialize and tries to start Wifi.
+ * @param ssid is the ssid of the network trying to be conected to.
+ * @param password is the password of the network trying to be conected to.
+ * @return ESP_OK if successful, otherwise an error code.
+**/
+esp_err_t wifi_sta(
+    uint8_t ssid[32], 
+    uint8_t password[64]);
+
+#if __cplusplus
+}
+#endif
+
+#endif //WIFI_H
