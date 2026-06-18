98be01f6f485e3fa385014291f34d8e6b9dcead5
Angular <gustavohenriquelima1@hotmail.com>
2026-06-18 11:02:07 -0300

    modified:   components/wifi/wifi.c 	modified:   main/main.c

--- components/wifi/wifi.c
+++ components/wifi/wifi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "wifi.h"
 
-static const char *TAG = "Wifi";
+static const char *TAG = "WIFI";
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -22,18 +22,22 @@ esp_err_t wifi_sta(uint8_t ssid[32], uint8_t password[64]){
     esp_err_t err = nvs_flash_init();
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to init nvs flash: %s", esp_err_to_name(err));
+        
         return err;
     }
 
     err = esp_netif_init();
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to init Netif: %s", esp_err_to_name(err));
+        nvs_flash_deinit();
         return err;
     }
 
     err = esp_event_loop_create_default();
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to create event loop: %s", esp_err_to_name(err));
+        esp_netif_deinit();
+        nvs_flash_deinit();
         return err;
     }
 
@@ -41,17 +45,29 @@ esp_err_t wifi_sta(uint8_t ssid[32], uint8_t password[64]){
     err = esp_wifi_init(&cfg);
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to init wifi: %s", esp_err_to_name(err));
+        esp_event_loop_delete_default();
+        esp_netif_deinit();
+        nvs_flash_deinit();
         return err;
     }
 
     err = esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL);
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to register wifi event handler: %s", esp_err_to_name(err));
+        esp_wifi_deinit();
+        esp_event_loop_delete_default();
+        esp_netif_deinit();
+        nvs_flash_deinit();
         return err;
     }
     err = esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL);
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to register ip event handler: %s", esp_err_to_name(err));
+        esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler);
+        esp_wifi_deinit();
+        esp_event_loop_delete_default();
+        esp_netif_deinit();
+        nvs_flash_deinit();
         return err;
     }
     // Initialize default station as network interface instance (esp-netif)
@@ -74,16 +90,34 @@ esp_err_t wifi_sta(uint8_t ssid[32], uint8_t password[64]){
     err = esp_wifi_set_mode(WIFI_MODE_STA);
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to set wifi mode: %s", esp_err_to_name(err));
+        esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler);
+        esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler);
+        esp_wifi_deinit();
+        esp_event_loop_delete_default();
+        esp_netif_deinit();
+        nvs_flash_deinit();
         return err;
     }
     err = esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to set wifi config: %s", esp_err_to_name(err));
+        esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler);
+        esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler);
+        esp_wifi_deinit();
+        esp_event_loop_delete_default();
+        esp_netif_deinit();
+        nvs_flash_deinit();
         return err;
     }
     err = esp_wifi_start();
     if(err != ESP_OK){
         ESP_LOGE(TAG, "Failed to set wifi config: %s", esp_err_to_name(err));
+        esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler);
+        esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler);
+        esp_wifi_deinit();
+        esp_event_loop_delete_default();
+        esp_netif_deinit();
+        nvs_flash_deinit();
         return err;
     }
 
--- main/main.c
+++ main/main.c
@@ -5,5 +5,6 @@ void app_main(void)
 {
     uint8_t ssid[32] = "GABRIEL";
     uint8_t password[64] = "Master1357@";
+    
     wifi_sta(ssid, password);
 }
